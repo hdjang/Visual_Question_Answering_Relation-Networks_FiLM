c6e59fdaee370fba610e7101bca3cbc29da04b0f
hdjang <hdjang@kaist.ac.kr>
2019-12-27 02:36:08 +0900

    Create eval_RNs.sh

---
+++ scripts/eval_RNs.sh
@@ -0,0 +1,12 @@
+CUDA_VISIBLE_DEVICES=0 \
+python main.py  --exp_id      -                \
+                --model       RN               \
+                --phase       test             \
+                --num_workers 40               \
+                --batch_size  128              \
+                \
+                --cnn_chs     128,128,128,128  \
+                --rn_g_chs    512,512          \
+                --rn_f_chs    512,512          \
+                \
+                --checkpoint  './model/RN.pth' \
